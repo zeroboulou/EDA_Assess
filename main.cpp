1c26f1d3d4078c5a18f4c7fe181e16aa54f45df0
Your Name <2740014552@qq.com>
2023-11-18 19:56:55 +0800

    EDA test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
-#include "placement/placement.cpp"
+
 #include "exporter/exporter.cpp"
 int main()
 {
     ReadFile();
     CreatFirstLayout();
     OptimizeLayout();
-
+    OutPut();
 }

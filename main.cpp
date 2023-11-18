6edea15d286bf9145036e4ec3fd2de058b32ab3c
Your Name <2740014552@qq.com>
2023-11-18 17:02:16 +0800

    test

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "placement/placement.cpp"
+int main()
+{
+    ReadFile();
+    CreatFirstLayout();
+    OptimizeLayout();
+    for (int i = 0; i < m.Instances; i)
+    {
+        cout<<"C"<<i 1<<" "<<"("<<layout[i 1].x<<","<<layout[i 1].y<<")"<<endl;
+    }
+
+}
